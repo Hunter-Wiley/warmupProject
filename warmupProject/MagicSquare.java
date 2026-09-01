8683d59a6219cac804ffe8c273db5137c7d15722
Hunter-Wiley <sigmaprime2018@gmail.com>
2026-09-01 15:45:08 -0700

    Add files via upload

---
+++ warmupProject/MagicSquare.java
@@ -0,0 +1,29 @@
+public class MagicSquare implements MagicSquareInterface{
+
+    @Override
+    public boolean isMagicSquare() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isMagicSquare'");
+    }
+
+    @Override
+    public int[][] getMatrix() {
+        //
+        throw new UnsupportedOperationException("Unimplemented method 'getMatrix'");
+    }
+
+    @Override
+    public String finalString() {
+        // returns a string showing the martix and whether or not it was successful.
+        System.out.println("The matrix:");
+        // find a way to have this be printed out properly. Ask Kount Learning Center
+        for (int i = 0; int[i][j], i++){ // Iterate through each row and column, printing out each object.
+            System.out.print("\t");
+            for (int j = 0; j < placeholderArray[i][j]; j++) {
+                System.out.print(placeholderArray[i][j] + " ");
+            }
+            System.out.println();
+            
+
+        }
+}
