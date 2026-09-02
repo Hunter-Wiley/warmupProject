7869be5335de60224f9131365e7f6a476caa4a8d
Hunter Wiley <Sigmaprime2018@gmail.com>
2026-09-01 18:03:02 -0700

    Making progress

--- warmupProject/MagicSquareDriver.java
+++ warmupProject/MagicSquareDriver.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MagicSquareDriver {
     // When creating a file make sure that the command line is odd.
     // Have some sort of try/catch statement for that?
@@ -9,4 +11,10 @@ public class MagicSquareDriver {
     // Third order: Check command-line arguments to both create/read.
     // Be sure to check to see if the filename already exists.
     // In addition, when creating a file have it print out the newly created file and see if it is a magic square.
+
+    // Use the given algorithm in the description of the assignment.
+    // if(totalNum / magicNum == magicNum(Math.pow(magicNum.pow,2)+1)/2){ //Maybe have a try catch statement for if it is a double, as that is an automatic failure.
+        // magicSquare = True
+        // Have an if statement printing out "is a magic square". If not, print "is not a magic square".
+    //}
 }
