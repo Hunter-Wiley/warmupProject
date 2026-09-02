ab2f08619d8c41436fd6543b0c4c55595f81992d
Hunter Wiley <sigmaprime2018@gmail.com>
2026-09-02 13:13:39 -0600

    Making HUGE progress

--- warmupProject/MagicSquare.java
+++ warmupProject/MagicSquare.java
@@ -1,9 +1,55 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class MagicSquare implements MagicSquareInterface{
+
+    File file = new File(filename);
+    PrintWriter outfile = new PrintWriter(file);
+    Boolean magicSquare = true;
+
+    public void MagicSquare(File "*.txt"){
+        try {
+            
+        } catch (Exception e) {
+            System.out.println("INVALID FILE FORMAT/FILE UNABLE TO BE OPENED!");
+        }
+    } 
 
     @Override
     public boolean isMagicSquare() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isMagicSquare'");
+        int magicNum = 0;
+        int totalNum = 0;
+
+        for (int row = 0; row < intArray[row]; row++) {
+            if(magicSquare = false){
+                break;
+            }
+            for (int col = 0; col < intArray[row][col]; col++) {
+            magicNum += intArray[row][col];
+            totalNum += intArray[row][col];
+            }
+            if(magicNum != magicNum * (Math.pow(magicNum,2) + 1) / 2){
+                magicSquare = false;
+            }
+        }
+        magicNum = 0;
+        for (int col = 0; col < intArray[col]; col++) {
+            if(magicSquare = false){
+                break;
+            }
+            for (int row = 0; row < intArray[row][col]; row++) {
+            magicNum += intArray[row][col];
+            }
+            if(magicNum != magicNum * (Math.pow(magicNum,2) + 1) / 2){
+                magicSquare = false;
+            }
+        }
+        if(totalNum / magicNum == magicNum * (Math.pow(magicNum,2)+1)/2){ // This makes sure that ALL integers are correct.
+            magicSquare = true;
+        }
     }
 
     @Override
@@ -12,18 +58,22 @@ public class MagicSquare implements MagicSquareInterface{
         throw new UnsupportedOperationException("Unimplemented method 'getMatrix'");
     }
 
-    @Override
+    @Override // Use the given algorithm in the description of the assignment.
     public String finalString() {
-        // returns a string showing the martix and whether or not it was successful.
+        // returns a string showing the matrix and whether or not it was successful.
         System.out.println("The matrix:");
         // find a way to have this be printed out properly. Ask Kount Learning Center
-        for (int i = 0; int[i][j], i++){ // Iterate through each row and column, printing out each object.
+        for (int i = 0; placeholder[i][j]; i++){ // Iterate through each row and column, printing out each object.
             System.out.print("\t");
             for (int j = 0; j < placeholderArray[i][j]; j++) {
                 System.out.print(placeholderArray[i][j] + " ");
             }
             System.out.println();
-            
-
         }
+        if(magicSquare = true){
+            System.out.println("is a magic square");
+        }else{
+            System.out.println("is not a magic square");
+        }
+    }
 }
--- warmupProject/MagicSquareDriver.java
+++ warmupProject/MagicSquareDriver.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MagicSquareDriver {
     // When creating a file make sure that the command line is odd.
@@ -7,14 +10,31 @@ public class MagicSquareDriver {
     // First order of business: Create a MagicSquare object
 
     // Second order: Have that object read from input
+    File file = new File(filename);
+    PrintWriter outfile = new PrintWriter(file);
 
     // Third order: Check command-line arguments to both create/read.
     // Be sure to check to see if the filename already exists.
     // In addition, when creating a file have it print out the newly created file and see if it is a magic square.
+    // Java will run the program from the command line as follows  -->  java MagicSquareDriver <-check | -create> <filename> < |size> 
+    // It uses args[i] to check what is being done. Ex: size = args[5]
+    
+    public static void main(String[] args) {
+        Boolean isRightCommand = false;
+        while(isRightCommand == false){
+            if((int)args[5] % 2 == 0){
+                System.out.println("Invalid size!");
+            }else{
+                int size = args[5]; // Use this for creating the Magic Square.
+            }
+        
+        }
 
-    // Use the given algorithm in the description of the assignment.
-    // if(totalNum / magicNum == magicNum(Math.pow(magicNum.pow,2)+1)/2){ //Maybe have a try catch statement for if it is a double, as that is an automatic failure.
-        // magicSquare = True
-        // Have an if statement printing out "is a magic square". If not, print "is not a magic square".
-    //}
+        //System.out.println(MagicSquare.finalString());
+        if(magicSquare == true){
+            System.out.println("is a magic square.");
+        }else{
+            System.out.println("is not a magic square.");
+        }
+    }
 }
