9f9ff60cf7287471415a3ba25ba261511505bf2f
Hunter Wiley <Sigmaprime2018@gmail.com>
2026-09-01 17:29:57 -0700

    Attempt 2

--- warmupProject/MagicSquareDriver.java
+++ warmupProject/MagicSquareDriver.java
@@ -2,5 +2,11 @@ public class MagicSquareDriver {
     // When creating a file make sure that the command line is odd.
     // Have some sort of try/catch statement for that?
 
-    // First order of business: Create a MagicSquare object and get output
+    // First order of business: Create a MagicSquare object
+
+    // Second order: Have that object read from input
+
+    // Third order: Check command-line arguments to both create/read.
+    // Be sure to check to see if the filename already exists.
+    // In addition, when creating a file have it print out the newly created file and see if it is a magic square.
 }
